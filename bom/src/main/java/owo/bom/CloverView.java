55c92cd819c8387e00903eb7462e51e3a90ae015
wangli <leeowenowen@gmail.com>
2016-08-12 22:37:59 +0800

    add CloverView

---
+++ bom/src/main/java/owo/bom/CloverView.java
@@ -0,0 +1,53 @@
+package owo.bom;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.Log;
+import android.view.View;
+
+
+public class CloverView extends View {
+    private Paint mPaint = new Paint();
+    Bitmap mMemBitmap;
+    private Canvas mMemCanvas;
+    private Path mPath = new Path();
+
+    public CloverView(Context context) {
+        super(context);
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(2);
+        mMemBitmap = Bitmap.createBitmap(800, 800, Bitmap.Config.ARGB_8888);
+        mMemCanvas = new Canvas(mMemBitmap);
+    }
+
+    /**
+     * x=sin(t)*sin(t)*cos(t)
+     * y=cos(t)*cos(t)*sin(t)
+     */
+    private void drawHeart(Canvas canvas) {
+        mPath.reset();
+        double a = 400;
+        double t = 0;
+        double x = 400 + a * Math.sin(t) * Math.sin(t) * Math.cos(t);
+        double y = 400 + a * Math.cos(t) * Math.cos(t) * Math.sin(t);
+        mPath.moveTo((float) x, (float) y);
+        for (; t < 2 * Math.PI; t += 0.01) {
+            x = 400 + a * Math.sin(t) * Math.sin(t) * Math.cos(t);
+            y = 400 + a * Math.cos(t) * Math.cos(t) * Math.sin(t);
+            mPath.lineTo((float) x, (float) y);
+            Log.d("XXX", "" + x + ":" + y);
+        }
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawHeart(mMemCanvas);
+        canvas.drawBitmap(mMemBitmap, 0, 0, null);
+    }
+}
--- demo/src/main/java/com/owo/bom/MainActivity.java
+++ demo/src/main/java/com/owo/bom/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import owo.bom.CloverView;
 import owo.bom.HeartView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        HeartView view = new HeartView(this);
+        CloverView view = new CloverView(this);
         view.setBackgroundColor(Color.GREEN);
         setContentView(view);
     }
