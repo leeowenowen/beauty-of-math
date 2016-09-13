ea722b8fcb795efb01d713e27012e060af93f153
wangli <leeowenowen@gmail.com>
2016-09-14 00:49:12 +0800

    finish bezier

--- bom/src/main/java/owo/bom/Bezier.java
+++ bom/src/main/java/owo/bom/Bezier.java
@@ -9,6 +9,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Bezier extends View {
@@ -55,22 +56,56 @@ public class Bezier extends View {
     @Override
     public void onDraw(Canvas canvas) {
         //近似值法按照趋近求值
-        if (controlPoints != null && controlPoints.length > 2) {
-            for (int i = 0; i <= pointNumber; i++) {
-                Point[] processPoints = controlPoints;
-                Point[] tmpProcessPoints = new Point[processPoints.length - 1];
-                while (tmpProcessPoints.length > 0) {
-                    for (int j = 0; j < tmpProcessPoints.length; j++) {
-                        Point t = null;
-                        t = getPointByRatio(processPoints[j], processPoints[j + 1], (float) i / (float) pointNumber);
-                        tmpProcessPoints[j] = t;
-                    }
-                    processPoints = tmpProcessPoints;
-                    tmpProcessPoints = new Point[processPoints.length - 1];
-                }
-                canvas.drawCircle(processPoints[0].x, processPoints[0].y, 5, paint);
-            }
+//        if (controlPoints != null && controlPoints.length > 2) {
+//            for (int i = 0; i <= pointNumber; i++) {
+//                Point[] processPoints = controlPoints;
+//                Point[] tmpProcessPoints = new Point[processPoints.length - 1];
+//                while (tmpProcessPoints.length > 0) {
+//                    for (int j = 0; j < tmpProcessPoints.length; j++) {
+//                        Point t = null;
+//                        t = getPointByRatio(processPoints[j], processPoints[j + 1], (float) i / (float) pointNumber);
+//                        tmpProcessPoints[j] = t;
+//                    }
+//                    processPoints = tmpProcessPoints;
+//                    tmpProcessPoints = new Point[processPoints.length - 1];
+//                }
+//                canvas.drawCircle(processPoints[0].x, processPoints[0].y, 5, paint);
+//            }
+//        }
+        List list = new ArrayList();
+        for (int i = 0; i < controlPoints.length; ++i) {
+            list.add(controlPoints[i]);
         }
+        draw(canvas, list);
+    }
+
+    public void draw(Canvas canvas, List<Point> controlPoints) {
+        for (int i = 0; i < 8; ++i) {
+            Point p = compute(controlPoints, i, 8);
+            canvas.drawCircle(p.x, p.y, 5, paint);
+        }
+    }
+
+    public Point compute(List<Point> controlPoints, int i, int n) {
+        List<Point> tmpPoints = controlPoints;
+        while (tmpPoints.size() > 2) {
+            List<Point> points = new ArrayList<>();
+            for (int j = 0; j < tmpPoints.size() - 1; j++) {
+                Point start = tmpPoints.get(j);
+                Point end = tmpPoints.get(j + 1);
+                points.add(computePoint(start, end, i, n));
+            }
+            tmpPoints = points;
+        }
+        return computePoint(tmpPoints.get(0), tmpPoints.get(1), i, n);
+
+    }
+
+    public Point computePoint(Point start, Point end, int i, int n) {
+        Point ret = new Point();
+        ret.x = start.x + (end.x - start.x) * i / n;
+        ret.y = start.y + (end.y - start.y) * i / n;
+        return ret;
     }
 
     private static Point getPointByRatio(Point startPoint, Point endPoint, float ratio) {
--- demo/src/main/java/com/owo/bom/MainActivity.java
+++ demo/src/main/java/com/owo/bom/MainActivity.java
@@ -1,9 +1,11 @@
 package com.owo.bom;
 
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import owo.bom.Bezier;
 import owo.bom.SampleBezierView;
 import owo.bom.SampleBezierViewLevel1;
 import owo.bom.WaterDropView;
@@ -14,15 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //   Debug.waitForDebugger();
-//    Bezier view = new Bezier(this);
-//    Point[] pts = new Point[] {
-//      new Point(100, 150), new Point(80, 100), new Point(50, 50), new Point(100, 0),
-//      new Point(150, 50), new Point(120, 100), new Point(100, 150),
-//      };
-//    view.setControlPoints(pts);
-        SampleBezierView view = new SampleBezierView(this);
+        Bezier view = new Bezier(this);
+        Point[] pts = new Point[]{
+//                new Point(100, 150), new Point(80, 100), new Point(50, 50), new Point(100, 0),
+//                new Point(150, 50), new Point(120, 100), new Point(100, 150),
+                new Point(0, 0), new Point(100, 150), new Point(300, 150), new Point(450, 0),
+        };
+        view.setControlPoints(pts);
+        //  SampleBezierView view = new SampleBezierView(this);
 
-        view.setBackgroundColor(Color.BLACK);
+        // view.setBackgroundColor(Color.BLACK);
         setContentView(view);
 
 
