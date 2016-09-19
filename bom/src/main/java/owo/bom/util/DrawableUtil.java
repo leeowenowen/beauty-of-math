c122adf480319f36f35d26f55546d3490222f92e
owenoranba <wl89397@alibaba-inc.com>
2016-09-19 19:49:42 +0800

    remove no use code

---
+++ bom/src/main/java/owo/bom/util/DrawableUtil.java
@@ -0,0 +1,21 @@
+package owo.bom.util;
+
+import android.content.res.ColorStateList;
+
+/**
+ * Created by wangli on 9/19/16.
+ */
+public class DrawableUtil {
+  private ColorStateList createColorStateList(int normal, int pressed, int focused, int unable) {
+    int[] colors = new int[] {pressed, focused, normal, focused, unable, normal};
+    int[][] states = new int[6][];
+    states[0] = new int[] {android.R.attr.state_pressed, android.R.attr.state_enabled};
+    states[1] = new int[] {android.R.attr.state_enabled, android.R.attr.state_focused};
+    states[2] = new int[] {android.R.attr.state_enabled};
+    states[3] = new int[] {android.R.attr.state_focused};
+    states[4] = new int[] {android.R.attr.state_window_focused};
+    states[5] = new int[] {};
+    ColorStateList colorList = new ColorStateList(states, colors);
+    return colorList;
+  }
+}
--- demo/src/main/java/com/owo/bom/MainActivity.java
+++ demo/src/main/java/com/owo/bom/MainActivity.java
@@ -1,16 +1,15 @@
 package com.owo.bom;
 
 import android.animation.ValueAnimator;
-import android.annotation.TargetApi;
 import android.content.res.ColorStateList;
 import android.graphics.Canvas;
 import android.graphics.PointF;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.Window;
+import android.view.WindowManager;
 
 import owo.bom.drawable.BaseDrawable;
 import owo.bom.splash.SplashDrawerDirector;
@@ -19,21 +18,18 @@ import owo.bom.util.WindowUtil;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
-  FrameLayout content;
-  private PointF center = new PointF();
 
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    this.getWindow()
+        .setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                  WindowManager.LayoutParams.FLAG_FULLSCREEN);
     //-------------------------------------------------
-    content = new FrameLayout(this);
-    setContentView(content);
     final View view = new View(this);
-    content.addView(view,
-                    new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-                                                 FrameLayout.LayoutParams.MATCH_PARENT));
-    //view.getLocationInWindow(center);
+    setContentView(view);
+    PointF center = new PointF();
     DisplayMetrics dm = WindowUtil.getScreenSize(this);
     center.x = dm.widthPixels / 2;
     center.y = dm.heightPixels / 2;
@@ -54,16 +50,4 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
-  private ColorStateList createColorStateList(int normal, int pressed, int focused, int unable) {
-    int[] colors = new int[] {pressed, focused, normal, focused, unable, normal};
-    int[][] states = new int[6][];
-    states[0] = new int[] {android.R.attr.state_pressed, android.R.attr.state_enabled};
-    states[1] = new int[] {android.R.attr.state_enabled, android.R.attr.state_focused};
-    states[2] = new int[] {android.R.attr.state_enabled};
-    states[3] = new int[] {android.R.attr.state_focused};
-    states[4] = new int[] {android.R.attr.state_window_focused};
-    states[5] = new int[] {};
-    ColorStateList colorList = new ColorStateList(states, colors);
-    return colorList;
-  }
 }
